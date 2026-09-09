30114d30763c16df05e2f0877aca5e74b316336f
Vahe <vaheg804@gmail.com>
2026-09-09 16:58:26 +0400

    creating an executable file and writing tests for functions in main.c

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include "my_string.h"
 
 int main(){
+
+    //1. strlen()
+    // char str[] = "Hello";
+    // printf("strlen() = %d",strlen(str));
+    //2. strcat()
+    // char s1[] = "Hello";
+    // char s2[] = " World";
+    // printf("strcat() = %s",strcat(s1,s2));
+    //3. strcpy()
+    // char s1[20] = "HI";
+    // char s2[] = "World";
+    // printf("strcpy() = %s",strcpy(s1,s2));
+    //4. strcmp()
+    // char s1[] = "b";
+    // char s2[] = "a";
+    // printf("strcmp() = %d",strcmp(s1,s2));
+    //5. strchr()
+    // char str[] = "Hello";
+    // printf("str[0] = %p\n",str);
+    // printf("strchr() = %p",strchr(str,'H'));
 
     return 0;
 }
