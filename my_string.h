acf3ec625238f9704add31f57c9d6706f9d6361d
Vahe <vaheg804@gmail.com>
2026-09-09 15:50:03 +0400

    Added the strcmp() function and added code.

--- my_string.h
+++ my_string.h
@@ -5,3 +5,5 @@ int strlen(char *string);
 char *strcat(char *destination, const char *source);
 //3. Copies a string to another string.
 char *strcpy(char *destination, const char *source);
+//4. compares two strings
+int strcmp( const char *s1, const char *s2 );
